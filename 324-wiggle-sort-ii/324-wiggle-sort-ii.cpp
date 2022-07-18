d42912aedc4c3067c49c080ce0c15ac7b5c7b448
Yeon <80302657+victoryAshe@users.noreply.github.com>
2022-07-18 18:32:48 +0900

    Time: 1187 ms (5.05%), Space: 17.4 MB (89.08%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -5,10 +5,10 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        if(nums.size()%2==0)
-            sort(nums.begin(), nums.begin()+nums.size()/2 , greater<int>());
-        else
-            sort(nums.begin(), nums.begin()+nums.size()/2+1 , greater<int>());
+        
+        vector<int>::iterator p;
+        p = nums.size()%2==0 ? nums.begin()+nums.size()/2 : nums.begin()+nums.size()/2+1;
+        sort(nums.begin(), p , greater<int>());
 
         
         int i = 1;
