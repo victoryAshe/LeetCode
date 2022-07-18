4bece173e2043989ac425d0b4c2acce489a1e81b
Yeon <80302657+victoryAshe@users.noreply.github.com>
2022-07-18 18:35:53 +0900

    Time: 2118 ms (5.05%), Space: 17.3 MB (96.34%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -9,18 +9,14 @@ public:
         vector<int>::iterator p;
         p = nums.size()%2==0 ? nums.begin()+nums.size()/2 : nums.begin()+nums.size()/2+1;
         sort(nums.begin(), p , greater<int>());
-
         
         int i = 1;
-        int min = nums.size();
-        while(i<=min)
+        while(i<=nums.size())
         {
             nums.insert(nums.begin()+i, nums[nums.size()-1]);
             nums.pop_back();
             i+=2;
         }
-        
-
         
     }
 };
