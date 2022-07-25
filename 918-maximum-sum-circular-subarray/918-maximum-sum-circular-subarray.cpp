28d5bc0dffb47f6ad2de48688130953ff31281a6
Yeon <80302657+victoryAshe@users.noreply.github.com>
2022-07-26 01:02:23 +0900

    Time: 131 ms (18.12%), Space: 41.5 MB (14.64%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-
-int great = -3 * pow(10,4);
-int minS = 3 * pow(10,4);
-
+    int great = -3 *pow(10,4);
+    int minS = 3*pow(10,4);
     
-int maxSubarraySumCircular(vector<int>& nums) {
-
-	if (nums.size() == 1) return nums[0];
-	vector<int> minNum = nums;
-
-	int sum = nums[0];
-	for (int i = 1; i < nums.size(); i++)
-	{
-		sum += nums[i];
-
-		minNum[i] = min(minNum[i], minNum[i] + minNum[i - 1]);
-		minS = min(minS, minNum[i]);
-
-		nums[i] = max(nums[i], nums[i] + nums[i - 1]);
-		great = max(great, nums[i]);
-	}
-
-	if (minS == sum) return great;
-	else sum -= minS;
-
-	return max(sum, great);
-
-}
-
+    int maxSubarraySumCircular(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        
+        vector<int> minNum = nums;
+        int sum = nums[0];
+        
+        for(int i = 1; i<nums.size(); i++)
+        {
+            sum+=nums[i];
+            
+            minNum[i] = min(minNum[i], minNum[i]+minNum[i-1]);
+            minS = min(minS,minNum[i]);
+            
+            nums[i] = max(nums[i], nums[i]+nums[i-1]);
+            great = max(great, nums[i]);
+            
+        }
+        
+        if(minS==sum) return great;
+        else sum-=minS;
+        
+        return max(sum, great);
+        
+    }
 };
