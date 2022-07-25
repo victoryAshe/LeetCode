53c57600ad1ed00973e50fa61c7fa69cfb73c52c
Yeon <80302657+victoryAshe@users.noreply.github.com>
2022-07-26 00:58:45 +0900

    Time: 138 ms (12.98%), Space: 41.5 MB (13.62%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,26 +1,27 @@
 
+
 class Solution {
 public:
 
 int great = -3 * pow(10,4);
 int minS = 3 * pow(10,4);
-    
 
+    
 int maxSubarraySumCircular(vector<int>& nums) {
 
-    if(nums.size()==1) return nums[0];
-    vector<int> minNum = nums;
+	if (nums.size() == 1) return nums[0];
+	vector<int> minNum = nums;
 
 	int sum = nums[0];
 	for (int i = 1; i < nums.size(); i++)
 	{
 		sum += nums[i];
-        
-        minNum[i] = min(minNum[i], minNum[i] + minNum[i-1]);
-        minS = min(minS, minNum[i]);
 
-        nums[i] = max(nums[i], nums[i] + nums[i-1]);
-        great = max(great, nums[i]);
+		minNum[i] = min(minNum[i], minNum[i] + minNum[i - 1]);
+		minS = min(minS, minNum[i]);
+
+		nums[i] = max(nums[i], nums[i] + nums[i - 1]);
+		great = max(great, nums[i]);
 	}
 
 	if (minS == sum) return great;
