1efa2dca77b507b320b5aee94b56a778868e6d52
Yeon <80302657+victoryAshe@users.noreply.github.com>
2022-11-15 15:43:49 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (55.81%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int d[50];
+
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(d[n]!=0) return d[n];
+
+        return d[n] = climbStairs(n-1)+climbStairs(n-2);
+    }
+};
