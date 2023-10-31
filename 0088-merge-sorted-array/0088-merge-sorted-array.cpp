0c517bc98ba029cd9042681869120227e2898844
Yeon <includeashe@gmail.com>
2023-10-31 19:19:35 +0900

    Time: 0 ms (100.00%), Space: 9.4 MB (54.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n==0) return;
-        if(m==0)
-        {
-            nums1 = nums2;
-            return;
-        }
         int p1, p2;
         p1 = nums1.size()-1;
         p2 = 0;
