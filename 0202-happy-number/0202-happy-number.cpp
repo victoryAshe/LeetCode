3b986f3e2063245ff466bb9f705f7b34232dcce6
Yeon <includeashe@gmail.com>
2023-11-06 01:54:40 +0900

    Time: 8 ms (9.87%), Space: 7.7 MB (9.66%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        
+        map<vector<int>, int> dict;
+        
+        while(true)
+        {
+            // 각 자리의 숫자를 추출
+            vector<int> temp;
+            while(n/10>0)
+            {
+                temp.push_back(n%10);
+                n/=10;
+            }
+            if(n>0) temp.push_back(n);
+            
+            // 현재 map에 해당 숫자들이 key로 존재하는지 검색
+            if(dict.find(temp)!=dict.end())
+            {
+                return false;
+            }
+            
+            
+            // 없다면, 각 자릿수의 제곱의 합을 계산
+            int val = 0;
+            for(int x: temp) val+= x*x;
+            
+            // happy인지 검사
+            if(val==1) return true;
+            
+            // happy가 아니면, 추가
+            dict.insert(make_pair(temp, val));
+            
+            // 다음 실행 준비
+            n = val;
+            
+            
+        }
+    }
+};
